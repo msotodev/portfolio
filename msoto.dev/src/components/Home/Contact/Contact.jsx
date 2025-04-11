119c5135e5d325c49f5bcced8254e1c8845315a9
msotodev <mariosotomor.dev@gmail.com>
2025-04-11 01:54:41 -0600

    Changed styles to inner children

--- msoto.dev/src/components/Home/Contact/Contact.jsx
+++ msoto.dev/src/components/Home/Contact/Contact.jsx
@@ -6,11 +6,11 @@ export default function Contact(){
     const { t } = useTranslation();
 
     return (
-        <section className="flex flex-col px-[25px] py-[45px] gap-[25px] items-center bg-[#e2e2e296]">
+        <section className="flex flex-col sm:px-[45px] px-[25px] py-[45px] gap-[25px] items-center bg-[#e2e2e296]">
             <IconTitle icon='/images/technologies/dark/contactme.png'
                     title={t("title-contact-me")} />
 
-            <div className="flex flex-col lg:flex-row gap-[15px] justify-center items-center text-md">
+            <div className="flex flex-col lg:flex-row gap-[15px] justify-center w-full items-center text-md">
                 <Link text={t("contact-mail")} href="mailto:mariosotomor.dev@gmail.com" image="/images/technologies/light/mail.png" />
                 <label className="text-[var(--primary-color)]">{t("or")}</label>
                 <Link text={t("contact-phone-number")} href="tel:3311395289" image="/images/technologies/light/phone.png" />
